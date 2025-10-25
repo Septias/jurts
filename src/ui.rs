b118f5e18f952df094115fb542a1a2bf080b66ce
Sebastian Klähn <info@sebastian-klaehn.de>
2025-10-25 18:17:17 +0900

    more ui elements

--- src/ui.rs
+++ src/ui.rs
@@ -47,18 +47,151 @@ fn ui_root() -> impl Bundle {
     (
         Node {
             width: percent(100),
-            height: percent(10),
+            height: percent(20),
             align_items: AlignItems::Start,
-            justify_content: JustifyContent::SpaceBetween,
+            justify_content: JustifyContent::Start,
             display: Display::Flex,
-            flex_direction: FlexDirection::Row,
+            flex_direction: FlexDirection::Column,
             padding: UiRect::all(px(10)),
             ..default()
         },
         children![
-            // Top left - Camera buttons
+            // Top bar row
             (
                 Node {
+                    width: percent(100),
+                    align_items: AlignItems::Start,
+                    justify_content: JustifyContent::SpaceBetween,
+                    display: Display::Flex,
+                    flex_direction: FlexDirection::Row,
+                    margin: UiRect::bottom(px(10)),
+                    ..default()
+                },
+                children![
+                    // Top left - Camera buttons
+                    (
+                        Node {
+                            display: Display::Flex,
+                            flex_direction: FlexDirection::Row,
+                            column_gap: px(5),
+                            ..default()
+                        },
+                        children![
+                            (
+                                button(
+                                    ButtonProps {
+                                        variant: ButtonVariant::Normal,
+                                        ..default()
+                                    },
+                                    (CameraControlButton {
+                                        axis: CameraAxis::X
+                                    },),
+                                    Spawn((Text::new("X"), ThemedText))
+                                ),
+                                observe(|_activate: On<Activate>| {
+                                    info!("X axis camera button clicked!");
+                                })
+                            ),
+                            (
+                                button(
+                                    ButtonProps {
+                                        variant: ButtonVariant::Normal,
+                                        ..default()
+                                    },
+                                    (CameraControlButton {
+                                        axis: CameraAxis::Y
+                                    },),
+                                    Spawn((Text::new("Y"), ThemedText))
+                                ),
+                                observe(|_activate: On<Activate>| {
+                                    info!("Y axis camera button clicked!");
+                                })
+                            ),
+                            (
+                                button(
+                                    ButtonProps {
+                                        variant: ButtonVariant::Normal,
+                                        ..default()
+                                    },
+                                    (CameraControlButton {
+                                        axis: CameraAxis::Z
+                                    },),
+                                    Spawn((Text::new("Z"), ThemedText))
+                                ),
+                                observe(|_activate: On<Activate>| {
+                                    info!("Z axis camera button clicked!");
+                                })
+                            ),
+                        ]
+                    ),
+                    // Top middle - Plane/Tent selection buttons
+                    (
+                        Node {
+                            display: Display::Flex,
+                            flex_direction: FlexDirection::Row,
+                            align_self: AlignSelf::Center,
+                            column_gap: px(5),
+                            ..default()
+                        },
+                        children![
+                            (
+                                button(
+                                    ButtonProps {
+                                        variant: ButtonVariant::Normal,
+                                        ..default()
+                                    },
+                                    (),
+                                    Spawn((Text::new("Plane"), ThemedText))
+                                ),
+                                observe(|_activate: On<Activate>| {
+                                    info!("Plane button clicked!");
+                                })
+                            ),
+                            (
+                                button(
+                                    ButtonProps {
+                                        variant: ButtonVariant::Normal,
+                                        ..default()
+                                    },
+                                    (),
+                                    Spawn((Text::new("Tent"), ThemedText))
+                                ),
+                                observe(|_activate: On<Activate>| {
+                                    info!("Tent button clicked!");
+                                })
+                            ),
+                        ]
+                    ),
+                    // Top right - Editing mode button
+                    (
+                        Node {
+                            ..Default::default()
+                        },
+                        children![(
+                            button(
+                                ButtonProps {
+                                    variant: ButtonVariant::Normal,
+                                    ..default()
+                                },
+                                (EditingModeButton,),
+                                Spawn((Text::new("Edit Mode: OFF"), ThemedText))
+                            ),
+                            observe(
+                                |_activate: On<Activate>, mut editing_mode: ResMut<EditingMode>| {
+                                    editing_mode.is_editing = !editing_mode.is_editing;
+                                    info!("Editing mode toggled: {}", editing_mode.is_editing);
+                                }
+                            )
+                        )]
+                    ),
+                ]
+            ),
+            // Second row - Jurte buttons (right-justified)
+            (
+                Node {
+                    width: percent(100),
+                    align_items: AlignItems::Center,
+                    justify_content: JustifyContent::End,
                     display: Display::Flex,
                     flex_direction: FlexDirection::Row,
                     column_gap: px(5),
@@ -71,13 +204,11 @@ fn ui_root() -> impl Bundle {
                                 variant: ButtonVariant::Normal,
                                 ..default()
                             },
-                            (CameraControlButton {
-                                axis: CameraAxis::X
-                            },),
-                            Spawn((Text::new("X"), ThemedText))
+                            (),
+                            Spawn((Text::new("Jurte"), ThemedText))
                         ),
                         observe(|_activate: On<Activate>| {
-                            info!("X axis camera button clicked!");
+                            info!("Jurte button clicked!");
                         })
                     ),
                     (
@@ -86,13 +217,11 @@ fn ui_root() -> impl Bundle {
                                 variant: ButtonVariant::Normal,
                                 ..default()
                             },
-                            (CameraControlButton {
-                                axis: CameraAxis::Y
-                            },),
-                            Spawn((Text::new("Y"), ThemedText))
+                            (),
+                            Spawn((Text::new("Hochjurte"), ThemedText))
                         ),
                         observe(|_activate: On<Activate>| {
-                            info!("Y axis camera button clicked!");
+                            info!("Hochjurte button clicked!");
                         })
                     ),
                     (
@@ -101,39 +230,14 @@ fn ui_root() -> impl Bundle {
                                 variant: ButtonVariant::Normal,
                                 ..default()
                             },
-                            (CameraControlButton {
-                                axis: CameraAxis::Z
-                            },),
-                            Spawn((Text::new("Z"), ThemedText))
+                            (),
+                            Spawn((Text::new("Großjurte"), ThemedText))
                         ),
                         observe(|_activate: On<Activate>| {
-                            info!("Z axis camera button clicked!");
+                            info!("Großjurte button clicked!");
                         })
                     ),
                 ]
-            ),
-            // Top right - Editing mode button
-            (
-                Node {
-                    // align_self: AlignSelf::End,
-                    ..Default::default()
-                },
-                children![(
-                    button(
-                        ButtonProps {
-                            variant: ButtonVariant::Normal,
-                            ..default()
-                        },
-                        (EditingModeButton,),
-                        Spawn((Text::new("Edit Mode: OFF"), ThemedText))
-                    ),
-                    observe(
-                        |_activate: On<Activate>, mut editing_mode: ResMut<EditingMode>| {
-                            editing_mode.is_editing = !editing_mode.is_editing;
-                            info!("Editing mode toggled: {}", editing_mode.is_editing);
-                        }
-                    )
-                )]
             ),
         ],
     )
